d4652d380520c0b926191fdd6a913431f88e0f03
frodinm <fabrizio_1197@hotmail.com>
2018-02-18 14:44:22 -0500

    Updates

--- src/components/divider.component.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from 'react';
-
-export class Divider extends PureComponent {
-  render() {
-    return (
-        <div id="divider" style={{alignSelf:'center'}}/>
-    );
-  }
-}
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export * from './header.component'
-export * from './divider.component'
-export * from './footer.component'
